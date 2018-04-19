869adae62cd4dbbc5c4d64c0503a41c570aad482
truongdx271 <truongdinh271@gmail.com>
2018-04-20 00:40:14 +0700

    Backbone commit

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://truongdx:231@ds149324.mlab.com:49324/devconnector"
+  mongoURI: "mongodb://truongdx:231@ds149324.mlab.com:49324/devconnector",
+  secretOrKey: "secret"
 };
